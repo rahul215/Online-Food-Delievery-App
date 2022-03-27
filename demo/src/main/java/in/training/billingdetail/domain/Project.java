504ad214952e7d44a3f322ef44416f5ade9c27df
rahul215 <rahul@DESKTOP-07E9RN0>
2022-03-27 23:34:54 +0530

    Project.class

--- demo/src/main/java/in/training/billingdetail/domain/Project.java
+++ demo/src/main/java/in/training/billingdetail/domain/Project.java
@@ -1,13 +1,53 @@
 package in.training.billingdetail.domain;
 
-import java.util.Date;
+import java.time.*;
 
 public class Project {
 	private String billId;
-	private Date billDate;
+	LocalDateTime billDate = LocalDateTime.now();
 	private int totalItem;
 	private double totalCost;
-	private OrderDetails order;
+	//private OrderDetails order;
+	
+	public Project(String billId, int totalItem, double totalCost) {
+		super();
+		this.billId = billId;
+		this.totalItem = totalItem;
+		this.totalCost = totalCost;
+	}
+
+	public String getBillId() {
+		return billId;
+	}
+
+	public void setBillId(String billId) {
+		this.billId = billId;
+	}
+
+	public LocalDateTime getBillDate() {
+		return billDate;
+	}
+
+	public void setBillDate(LocalDateTime billDate) {
+		this.billDate = billDate;
+	}
+
+	public int getTotalItem() {
+		return totalItem;
+	}
+
+	public void setTotalItem(int totalItem) {
+		this.totalItem = totalItem;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+	 
 	
 	
 }
