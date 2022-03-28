5f8fadce2db4d74aa06d51d67964d2b9ce322784
rahul215 <rahul@DESKTOP-07E9RN0>
2022-03-28 09:42:00 +0530

    OrderDetailNotFoudException

---
+++ demo/src/main/java/in/training/billingdetail/exception/OrderDetailNotFoundException.java
@@ -0,0 +1,21 @@
+package in.training.billingdetail.exception;
+
+public class OrderDetailNotFoundException {
+	
+	private static final long serialVersionUID=1L;
+
+    /**
+     * This will create the trainingNotfoundException object without error message
+     */
+    public OrderDetailNotFoundException() {
+        super();
+    }
+
+    /**
+     * This will create the trainingNotFoundException object with error message
+     */
+    public OrderDetailNotFoundException(String errMsg) {
+        super();
+    }
+
+}
