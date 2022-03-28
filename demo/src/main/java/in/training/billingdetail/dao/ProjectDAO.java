8c5c77cdee06b1d67474c5629eea7b23b052b1fd
rahul215 <rahul@DESKTOP-07E9RN0>
2022-03-28 09:49:45 +0530

    method interface

---
+++ demo/src/main/java/in/training/billingdetail/dao/ProjectDAO.java
@@ -0,0 +1,25 @@
+package in.training.billingdetail.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import in.training.billingdetail.domain.Bill;
+import in.training.billingdetail.exception.OrderDetailNotFoundException;
+
+public interface ProjectDAO {
+	
+	void addBill(Bill bill);
+	
+	void updateBill(Bill bill);
+	
+	void removeBill(Bill bill) throws OrderDetailNotFoundException;
+	
+	void viewBill(Bill bill);
+	
+	List<Bill> viewBills(LocalDate startDate, LocalDate endDate);
+	
+	List<Bill> viewBills(String custId);
+	
+	double calculateTotalCost(Bill bill);
+
+}
