ff9a490ee4b759480389b46c7606614d82c8f620
rishav2901 <87636907+rishav2901@users.noreply.github.com>
2022-03-31 09:14:09 +0530

    Add files via upload

---
+++ spring2/ppmtoolapi/src/main/java/in/capgimini/onlinefooddeliveryapplication/service/addressservice.java
@@ -0,0 +1,5 @@
+package in.capgimini.onlinefooddeliveryapplication.service;
+
+public class addressservice {
+
+}
---
+++ spring2/ppmtoolapi/src/test/java/in/codegram/ppmtoolapi/PpmtoolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codegram.ppmtoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
